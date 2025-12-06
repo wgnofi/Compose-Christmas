e5427c5449dcf9101cd64240e17d4c4f92595599
Raghul A <sraraghul@gmail.com>
2025-12-06 09:32:48 +0530

    DaySix Changes

--- app/src/main/java/com/raghul/composechristmas/DayTwo.kt
+++ app/src/main/java/com/raghul/composechristmas/DayTwo.kt
@@ -3,6 +3,7 @@ package com.raghul.composechristmas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -11,52 +12,95 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.star
 import androidx.graphics.shapes.toPath
 import com.raghul.composechristmas.ui.theme.ComposeChristmasTheme
 
 @Composable
 fun Tree() {
+    val roseBase = Color(183, 110, 121)
+    val lightPink = Color(255, 182, 193)
+    val highlight = Color(255, 240, 245)
+    val gold = Color(255,215, 0)
+    val metalGold = Color(211, 175, 55)
+    val oldGold = Color(208, 182, 61)
+    val listZeroBauble = listOf(gold, metalGold, oldGold, oldGold, metalGold, gold)
+    val listOneBauble = listOf(roseBase, lightPink, highlight, lightPink, roseBase)
+    val deepBlue = Color(0, 102, 153)
+    val vibrantCyan = Color(0, 153, 204)
+    val electricAqua = Color(102, 255, 255)
+    val listTwoBauble = listOf(deepBlue, vibrantCyan, electricAqua, vibrantCyan, deepBlue)
+    val darkGreen = Color(1, 48, 33)
+    val kellyGreen = Color(75, 181, 67)
+    val brightLime = Color(176, 255, 80)
+    val listThreeBauble = listOf(brightLime ,darkGreen, kellyGreen, brightLime, kellyGreen,brightLime)
+
     Box(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize().drawWithCache {
-            val brush = Brush.verticalGradient(
-                colors = listOf(Color(0xFF00003C), Color.Black)
-            )
-            onDrawBehind {
-                drawRect(brush)
-            }
-        })
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .drawWithCache {
+                val brush = Brush.verticalGradient(
+                    colors = listOf(Color(0xFF00003C), Color(0, 0, 10))
+                )
+                onDrawBehind {
+                    drawRect(brush)
+                }
+            })
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .trunk())
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .snowGround())
         Box(modifier = Modifier
             .align(alignment = Alignment.TopStart)
-            .offset(x = 50.dp,y = 200.dp)
-            .size(13.dp).drawStar(color = Color.White.copy(alpha = 0.5f)))
+            .offset(x = 50.dp, y = 200.dp)
+            .size(13.dp)
+            .drawStar(color = Color.White.copy(alpha = 0.5f)))
         Box(modifier = Modifier
             .align(alignment = Alignment.TopStart)
-            .offset(x = 100.dp,y = 50.dp)
-            .size(13.dp).drawStar(color = Color.White))
+            .offset(x = 100.dp, y = 50.dp)
+            .size(13.dp)
+            .drawStar(color = Color(0xFFFFFFE0)))
         Box(modifier = Modifier
             .align(alignment = Alignment.TopCenter)
-            .offset(x = 100.dp,y = 50.dp)
-            .size(10.dp).drawStar(color = Color.White))
+            .offset(x = 20.dp, y = 10.dp)
+            .size(10.dp)
+            .drawStar(color = Color.White.copy(alpha = 0.5f)))
         Box(modifier = Modifier
             .align(alignment = Alignment.TopCenter)
-            .offset(x = 10.dp,y = 20.dp)
-            .size(13.dp).drawStar(color = Color.White.copy(alpha = 0.5f)))
+            .offset(x = 100.dp, y = 50.dp)
+            .size(10.dp)
+            .drawStar(color = Color.White))
+        Box(modifier = Modifier
+            .align(alignment = Alignment.TopStart)
+            .offset(x = 10.dp, y = 120.dp)
+            .size(12.dp)
+            .drawStar(color = Color.White.copy(alpha = 0.5f)))
         Box(modifier = Modifier
             .align(alignment = Alignment.TopEnd)
-            .offset(x = (-20).dp,y = 100.dp)
-            .size(12.dp).drawStar(color = Color.White))
-        Box(modifier = Modifier.fillMaxSize().trunk())
-        for (i in 7 downTo 0) {
+            .offset(x = -10.dp, y = 220.dp)
+            .size(12.dp)
+            .drawStar(color = Color.White.copy(alpha = 0.5f)))
+        Box(modifier = Modifier
+            .align(alignment = Alignment.TopEnd)
+            .offset(x = (-20).dp, y = 100.dp)
+            .size(12.dp)
+            .drawStar(color = Color.White))
+        for (i in 6 downTo 0) {
             val verticalShift = 0.08f * i
             val widthFactor = 0.55f + (0.05f * i)
             val colorFactor = i * 10
@@ -71,9 +115,91 @@ fun Tree() {
                     )
             )
         }
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = 70.dp, y = (-120).dp)
+                .size(30.dp)
+                .baubles(ballColors = listThreeBauble, tipColor = Color(0, 34, 23))
+        )
+        Box(modifier = Modifier
+            .align(alignment = Alignment.TopCenter)
+            .offset(y = 70.dp)
+            .size(70.dp)
+            .radialStar()
+        )
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = (-60).dp, y = (-50).dp)
+                .size(30.dp)
+                .baubles(ballColors = listZeroBauble, tipColor = Color(148, 114, 20))
+        )
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = (-5).dp, y = 20.dp)
+                .size(30.dp)
+                .baubles(ballColors = listTwoBauble, tipColor = Color(0, 71, 107))
+        )
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = 60.dp, y = 90.dp)
+                .size(30.dp)
+                .baubles(ballColors = listOneBauble, tipColor = Color(128, 77, 85))
+        )
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = (-70).dp, y = 150.dp)
+                .size(30.dp)
+                .baubles(ballColors = listTwoBauble, tipColor = Color(0, 71, 107))
+        )
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .offset(x = 30.dp, y = 220.dp)
+                .size(30.dp)
+                .baubles(ballColors = listThreeBauble, tipColor = Color(0, 34, 23))
+        )
     }
 }
 
+@Composable
+fun Modifier.snowGround():Modifier  = this.drawWithCache {
+    val pureWhite = Color(255, 255, 255)
+    val snowWhite = Color(255, 250, 250)
+    val whitesmoke = Color(245, 245, 245)
+    val azureishWhite = Color(208, 224, 249)
+
+    onDrawBehind {
+            val width = size.width
+            val height = size.height
+
+            val bottomLeft = Offset(x = 0F, y = height)
+            val bottomRight = Offset(x = width, y = height)
+
+            val magnet = Offset(x = width / 2, y = height * 0.8f)
+            val path = Path().apply {
+                moveTo(bottomLeft.x, bottomLeft.y)
+                quadraticTo(magnet.x, magnet.y, bottomRight.x, bottomRight.y)
+                lineTo(bottomLeft.x, bottomLeft.y)
+                close()
+            }
+                drawPath(path, brush = Brush.horizontalGradient(
+                    colorStops = arrayOf(
+                        0.0f to snowWhite,
+                        0.1f to whitesmoke,
+                        0.4f to azureishWhite,
+                        0.6f to azureishWhite,
+                        0.9f to whitesmoke,
+                        1.0f to pureWhite
+                    )
+                ))
+
+    }
+}
 @Composable
 fun Modifier.jaggeredCone(
     jaggeredOffset: Float,
@@ -81,12 +207,13 @@ fun Modifier.jaggeredCone(
     topYPercent: Float,
     bottomYPercent: Float,
     color: Color
-): Modifier = this.drawWithCache {
+): Modifier = this
+    .drawWithCache {
         val width = size.width
         val height = size.height
 
-        val top = Offset(x = width /2 ,y = height * topYPercent )
-        val bottomRight = Offset(x = width * rightOffset, y= height * bottomYPercent)
+        val top = Offset(x = width / 2, y = height * topYPercent)
+        val bottomRight = Offset(x = width * rightOffset, y = height * bottomYPercent)
         val bottomLeft = Offset(x = width * (1 - rightOffset), y = height * bottomYPercent)
 
         val vertices = mutableListOf<Float>()
@@ -114,13 +241,16 @@ fun Modifier.jaggeredCone(
         vertices.add(bottomLeft.x)
         vertices.add(bottomLeft.y)
 
-        val roundedPolygon = RoundedPolygon(vertices = vertices.toFloatArray(),
-            rounding = CornerRounding(radius = 30f, smoothing = 0.5f))
+        val roundedPolygon = RoundedPolygon(
+            vertices = vertices.toFloatArray(),
+            rounding = CornerRounding(radius = 30f, smoothing = 0.5f)
+        )
         val roundedPath = roundedPolygon.toPath().asComposePath()
         onDrawBehind {
             drawPath(roundedPath, color = color)
         }
-    }.fillMaxSize()
+    }
+    .fillMaxSize()
 
 @Composable
 fun Modifier.trunk(
@@ -131,8 +261,8 @@ fun Modifier.trunk(
 
     val topLeft = Offset(x = width * 0.45f, y = height * 0.4f)
     val topRight = Offset(x = width * 0.55f, y = height * 0.4f)
-    val bottomLeft = Offset(x = width * 0.38f, y = height * 0.97f)
-    val bottomRight = Offset(x = width * 0.62f, y = height * 0.97f)
+    val bottomLeft = Offset(x = width * 0.38f, y = height * 0.95f)
+    val bottomRight = Offset(x = width * 0.62f, y = height * 0.95f)
     val vertices = mutableListOf<Float>()
     vertices.add(topLeft.x)
     vertices.add(topLeft.y)
@@ -179,6 +309,55 @@ fun Modifier.drawStar(
     }
 }
 
+@Composable
+fun Modifier.baubles(
+    ballColors: List<Color>,
+    tipColor: Color
+    ): Modifier = this.drawWithCache {
+    onDrawBehind {
+        drawRect(color = tipColor, topLeft =  Offset(x = size.width * 0.475f, y = size.height * 0.03f), size = Size(width = size.width * 0.04f, height = size.height * 0.2f))
+        translate(top = size.height / 7) {
+            drawRoundRect(
+                color = ballColors.random(),
+                topLeft = Offset(x = size.width * 0.415f, y = size.height * 0.05f),
+                size = size.div(6f),
+                cornerRadius = CornerRadius(x = 20f, y = 20f)
+            )
+            drawCircle(brush = Brush.sweepGradient(colors = ballColors), radius = size.maxDimension / 3)
+        }
+    }
+}
+@Composable
+fun Modifier.radialStar(
+
+): Modifier = this.drawWithCache {
+    val starPolygon = RoundedPolygon.star(
+        numVerticesPerRadius = 5,
+        innerRadius = size.minDimension / 5,
+        centerX = size.width / 2,
+        centerY = size.height / 2,
+        radius = size.minDimension / 2
+    )
+    val starPath = starPolygon.toPath().asComposePath()
+    onDrawBehind {
+        rotate(degrees = -90f) {
+            drawPath(
+                starPath,
+                brush = Brush.radialGradient(colors = listOf(Color.White, Color(255, 205, 0)))
+            )
+        }
+    }
+}
+
+//@Preview
+//@Composable
+//private fun StarPreview() {
+//    ComposeChristmasTheme {
+//        Box(modifier = Modifier
+//            .size(200.dp)
+//            .radialStar())
+//    }
+//}
 @Preview
 @Composable
 private fun TreePreview() {
